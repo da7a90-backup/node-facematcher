fbe87fb94f59b9a7a97ec23418abd24087d0076b
da7a90-backup <sidbarrack@gmail.com>
2023-07-24 02:45:17 +0000

    Feat: increasing payload size

--- index.js
+++ index.js
@@ -30,9 +30,8 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
- 
- app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({extended: true, limit: '50mb'}));
 
 app.use(cors())
 
